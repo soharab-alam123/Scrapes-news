7b0dcfe3fff9d2f7ce36afba4af21f361f755e07
Soharab Alam <142098414+soharab-alam123@users.noreply.github.com>
2026-05-07 16:20:48 +0530

    live url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://scrapes-news-backend.onrender.com'
 });
 
 API.interceptors.request.use((config) => {
